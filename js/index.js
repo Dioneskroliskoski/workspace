650cf0c9d9992b4f59e98af4c2683c9a879ab65e
Dioneskroliskoski <kroliskoski@outlook.com>
2023-02-25 19:27:25 -0300

    Este es mi primer commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const resultado = 5 === 6
+const resultado1 = '5'== '5'
+console.log(resultado1);
+let x = false
+while (x) {
+	console.log(x);
+	x = false
+}
+console.log('terminado el loop');
+
+switch () {
+	case 1: {
+
+	}
+	case 2: 
+}
